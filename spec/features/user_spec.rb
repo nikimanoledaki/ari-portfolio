2ef9623c260af9b6a1786a72cf281539f27b5176
Niki Manoledaki <18622989+nikimanoledaki@users.noreply.github.com>
2020-06-03 17:57:08 +0100

    Landing page with Enter link

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.feature 'Landing Page', type: :feature do
     scenario 'user can view landing page' do
         visit '/'
-        expect(page.find('Enter'))
+        expect(page.find_link('Enter'))
     end
 end
