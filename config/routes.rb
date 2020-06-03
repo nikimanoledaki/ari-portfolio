8af6f68962b4544f851b7e7949bb6cf023209d14
Niki Manoledaki <18622989+nikimanoledaki@users.noreply.github.com>
2020-06-03 18:57:48 +0100

    Routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   get "/", to: "welcome#index", as: "root"
-  get "/film", to: "category#film"
+  get "/film", to: "categories#index"
 end
--- spec/features/user_journey_spec.rb
+++ spec/features/user_journey_spec.rb
@@ -4,6 +4,6 @@ RSpec.feature 'Landing Page', type: :feature do
     scenario 'user can view landing page' do
         visit '/'
         click_link 'Enter'
-        expect(page).to have_content ('Ari')
+        expect(page).to have_content ('ARI SALAZAR')
     end
 end
