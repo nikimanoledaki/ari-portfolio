26877f6bc94ffcc5f40b49d390b9116bdfd918f5
Niki Manoledaki <18622989+nikimanoledaki@users.noreply.github.com>
2020-06-04 21:16:38 +0100

    Replaces categories with film

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get "/", to: "welcome#index", as: "root"
   
-  get "/film", to: "categories#index"
+  get "/film", to: "film#index"
   get "/documentary", to: "documentary#index"
   get "/fashion", to: "fashion#index"
 
